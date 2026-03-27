00486ad224cb28f5107efa13b6cfb6b320238db9
martin <martin.lemee@apollossc.com>
2026-03-27 10:19:29 +0100

    fix: update game phase when game starts

--- packages/client/src/hooks/useSocket.ts
+++ packages/client/src/hooks/useSocket.ts
@@ -56,6 +56,7 @@ export function useSocket(): {
 
     socket.on('game:started', () => {
       console.log('Game started');
+      setGamePhase('PROGRAMMING' as GamePhase, 0);
     });
 
     socket.on('game:phase', (phase: GamePhase, register: number) => {
